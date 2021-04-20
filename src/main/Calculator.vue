19b806a97d11236e1fdbd85667f66ea06eecb020
Diego Thomas <diegopthomas@gmail.com>
2021-04-20 13:40:11 -0300

    v1

---
+++ src/main/Calculator.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="calculator">
+      <Display value="1000" />
+      <Button label="AC" triple v-on:onClick="clearMemory" />
+      <Button label="/" operation v-on:onClick="setOperation"/>
+      <Button label="7" v-on:onClick="addDigit" />
+      <Button label="8" />
+      <Button label="9" />
+      <Button label="*" operation />
+      <Button label="4" />
+      <Button label="5" />
+      <Button label="6" />
+      <Button label="-" operation />
+      <Button label="1" />
+      <Button label="2" />
+      <Button label="3" />
+      <Button label="+" operation />
+      <Button label="0" double />
+      <Button label="." />
+      <Button label="=" operation />
+
+  </div>
+</template>
+
+<script>
+import Display from "../components/Display"
+import Button from "../components/Button"
+
+export default {
+    components: { Button, Display },
+    mothods: {
+        clearMemory() {
+            alert('Limpar memória')
+        },
+        setOperation(operation) {
+            console.log('Operação' + operation)
+        },
+
+        addDigit(n) {
+            console.log('Digito' + n)
+        }
+    }
+
+}
+
+</script>
+
+<style>
+    .calculator {
+        height: 320px;
+        width: 235px;
+        border-radius: 5px;
+        overflow: hidden;
+
+        display: grid;
+        grid-template-columns: repeat(4, 25%);
+        grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+    }
+</style>
